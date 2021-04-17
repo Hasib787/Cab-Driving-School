8648a5f56d6e78b20ef588004ff477e8b47271e8
Hasib787 <mr.hasib45@gmail.com>
2021-04-17 16:37:14 +0600

    add some router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound'
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({})
   return (
-    <div>
-        
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="*">
+          <NotFound />
+        </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
