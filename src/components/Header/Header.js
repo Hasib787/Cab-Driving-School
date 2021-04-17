1a337df591942d942e3c7815ef1e5bf4ed3171ec
Hasib787 <mr.hasib45@gmail.com>
2021-04-17 16:52:15 +0600

    added some chnage on nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
+        <div className="header-container">
+            {/* Nav Start  */}
+            <nav className="container navbar navbar-expand-lg navbar-light">
+
+                <div className="container d-flex justify-content-right">
+                    <div><h2>CAB DRIVING SCHOOL</h2></div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                        <ul className="navbar-nav  fw-bold">
+                            <li className="nav-item">
+                                <p><a><Link to="/home">Home</Link></a></p>
+                            </li>
+                            <li className="nav-item">
+                                <p> <a><Link to="/orders">Orders</Link></a></p>
+                            </li>
+                            <li className="nav-item">
+                                <p> <a><Link to="/admin">Admin</Link></a></p>
+                            </li>
+                            <li className="nav-item">
+                                <p><a><Link to="/deals">Deals</Link></a></p>
+                            </li>
+                            <li className="nav-item">
+
+                                <Link to="/login"><Button id="btn" variant="contained" >Login</Button>
+                                </Link>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {/* <!-- nav end --> */}
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <h3>This is home</h3>
         </div>
     );
