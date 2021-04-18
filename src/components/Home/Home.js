c7f027361e69c322d45740b6041561c8fb2d3a2d
Hasib787 <mr.hasib45@gmail.com>
2021-04-18 14:39:03 +0600

    added loader components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,20 +6,7 @@ const Home = () => {
     return (
         <div>
             <Header></Header>
-            <div className="loader">
-                <div class="dot dot-1"></div>
-                <div class="dot dot-2"></div>
-                <div class="dot dot-3"></div>
-            </div>
-
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-                <defs>
-                    <filter id="goo">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7" />
-                    </filter>
-                </defs>
-            </svg>
+            
         </div>
     );
 };
