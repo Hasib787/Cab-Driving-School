7b06a31e0d40459053b95847672f61de261977df
Hasib787 <mr.hasib45@gmail.com>
2021-04-19 17:52:59 +0600

    added some changes

--- src/components/Programs/Programs.js
+++ src/components/Programs/Programs.js
@@ -2,11 +2,11 @@ import React from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Button, Card } from 'react-bootstrap';
-import testImg from '../../images/programSlider3.png';
+import testImg from '../../images/car-blue.jpg';
 import './Programs.css';
 
 const Programs = () => {
-    
+
     return (
         <section>
             <div>
@@ -14,45 +14,20 @@ const Programs = () => {
             </div>
             <div className="mt-5  row col-md-12 col-sm-12">
                 <div className=" col-lg-4 col-md-6 col-sm-12 mb-3 d-flex justify-content-center">
-                <Card  className="border-0" style={{ width: '28rem' }}>
-                    <Card.Img id="imgCard" variant="top" src={testImg} />
-                    <Card.Body>
-                        <Card.Title>Adult In-Car Lessons</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                    <Card className="cardItem border-0 shadow-lg p-3 mb-5 bg-white rounded" style={{ width: '28rem' }}>
+                        <Card.Img id="imgCard" variant="top" src={testImg} />
+                        <Card.Body>
+                            <Card.Title className="mt-3">Adult In-Car Lessons</Card.Title>
+                            <Card.Text>
+                                All Inclusive programs provide the student with 30 hours of class, and all class materials. We offer weekday classes,
+                                Saturday/Sunday classes, Saturday only, and Sunday only classes
                         </Card.Text>
-                        <Button variant="primary">Book Now</Button>
-                    </Card.Body>
-                </Card>
+                            <h4>$</h4>
+                            <Button id="btn-order" variant="primary">Book Now</Button>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12  mb-3 d-flex justify-content-center">
-                <Card  style={{ width: '28rem' }}>
-                    <Card.Img variant="top" src={testImg} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12  mb-3 d-flex justify-content-center">
-                <Card  style={{ width: '28rem' }}>
-                    <Card.Img variant="top" src={testImg} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                </div>
-        </div>
         </section>
     );
 };
