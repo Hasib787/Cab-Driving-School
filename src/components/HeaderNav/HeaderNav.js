4e96086869ae5059729c025eef93e149eef2207b
Hasib787 <mr.hasib45@gmail.com>
2021-04-20 05:06:09 +0600

    added some changes

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -16,7 +16,7 @@ const HeaderNav = () => {
                         <Nav.Link id="about" className="active ms-4 " href="#aboutus">About us</Nav.Link>
                         <Nav.Link id="program" className="active ms-4" href="#programs">Programs</Nav.Link>
                         <Nav.Link id="blog" className="active ms-4" href="#link">Blogs</Nav.Link>
-                        <Nav.Link id="testimonial" className="active ms-4" href="#link">Testimonial</Nav.Link>
+                        <Nav.Link id="testimonial" className="active ms-4" href="#testimonials">Testimonial</Nav.Link>
                         <Link to="/admin"><Nav.Link id="admin" className=" ms-4" href="#admin">Admin</Nav.Link></Link>
                         <Link to="/login"><Button className="ms-5 me-4 text-light  btn-warning">Login</Button></Link>
                     </Nav>
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -6,7 +6,7 @@ import car from '../../images/slider/car-4.jpg';
 
 const Testimonial = () => {
     return (
-        <div className="testimonial mt-5">
+        <div id="testimonials" className="testimonial mt-5">
             <div className="t-testimonial text-center">
                 <h1>TESTIMONIALS</h1>
             </div>
@@ -16,7 +16,7 @@ const Testimonial = () => {
                     <Carousel>
                         <Carousel.Item>
                             <div className="review-card border-0 card">
-                                <div className="card-body">
+                                <div className="tbody">
                                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis quibusdam minus debitis repellat quidem! Maxime error voluptatem fuga dicta! Magnam autem impedit commodi sed nemo</p>
                                     <div className="card-title"><h5>Hasib</h5></div>
                                     <div className="mb-2 text-muted card-subtitle">
