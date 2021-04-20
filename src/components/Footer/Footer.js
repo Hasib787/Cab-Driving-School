8d2c900b99aa27e5e260bf557dc153cfc5844b3c
Hasib787 <mr.hasib45@gmail.com>
2021-04-20 06:01:32 +0600

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
+        <div className="footer text-center mt-5 row col-lg-12">
+            <div className="col-md-6 col-sm-12">
+                <h1>Address</h1>
+                <p>Office:- 59/F </p>
+                <p>TDM Mohona Tower (2nd floor) </p>
+                <p>West Rajabazar, Panthapath, Dhaka-1215</p>
+            </div>
+            <div className="col-md-6 col-sm-12">
+                <h3 style={{color:'orange'}}>CONNECT WITH US</h3>
+            </div>
+            <p id="copyRight">COPY ©2021 Cab Driving School</p>
         </div>
     );
 };
