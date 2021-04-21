25e0adea2c1b34b22f03175923bb66fadf4d4c28
Hasib787 <mr.hasib45@gmail.com>
2021-04-21 22:11:49 +0600

    added a selected form

--- src/components/Admin/AdminDashboard/OrderList/OrderListDetails.js
+++ src/components/Admin/AdminDashboard/OrderList/OrderListDetails.js
@@ -5,25 +5,29 @@ const OrderListDetails = () => {
     return (
         <div>
             <div>
-            <div>
-                <h3 className="mb-4 mt-5 text-left text-brand rounded bg-light p-3">
-                    Order List</h3>
+                <div>
+                    <h3 className="mb-4 mt-5 text-left text-brand rounded bg-light p-3">
+                        Order List</h3>
+                </div>
+                <div className="row text-center  manage-list-top mt-5 mx-0">
+                    <div className="col-2"><strong>Name</strong></div>
+                    <div className="col-2"><strong>Email ID</strong></div>
+                    <div className="col-2"><strong>Service</strong></div>
+                    <div className="col-2"><strong>Pay With</strong></div>
+                    <div className="col-2"><strong>Status</strong></div>
+                </div>
+                <div className="row mt-3 text-center bg-light manage-list-top m-0">
+                    <div className="col-2">Name</div>
+                    <div className="col-2">Email</div>
+                    <div className="col-2">Package Name</div>
+                    <div className="col-2">Price</div>
+                    <div className="col-2"><select class="form-select" aria-label="Default select example">
+                        <option style={{color:'red'}} value="1">Pending</option>
+                        <option style={{color:'orange'}} value="2">Ongoing</option>
+                        <option style={{color:'green'}} value="3">Done</option>
+                    </select></div>
+                </div>
             </div>
-            <div className="row text-center manage-list-top mt-5 mx-0">
-                <div className="col-2"><strong>Name</strong></div>
-                <div className="col-2"><strong>Email ID</strong></div>
-                <div className="col-2"><strong>Service</strong></div>
-                <div className="col-2"><strong>Pay With</strong></div>
-                <div className="col-2"><strong>Status</strong></div>
-            </div>
-            <div className="row mt-3 text-center bg-white manage-list-top m-0">
-                <div className="col-2"><strong>Name</strong></div>
-                <div className="col-2"><strong>Email</strong></div>
-                <div className="col-2"><strong>Package Name</strong></div>
-                <div className="col-2"><strong>Price</strong></div>
-                <div className="col-2"><strong><button className="btn btn-success m-1">pending</button></strong></div>
-            </div>
-        </div>
         </div>
     );
 };
